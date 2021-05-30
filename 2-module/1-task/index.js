728c1547b2455b24a9b67782488810e26166d68e
ann wi <vasileva.ana@outlook.com>
2021-05-30 14:32:03 +0300

    написала функцию sumSalary

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let John = salaries["John"];
+  let Ann = salaries["Ann"];
+  let Pete = salaries["Pete"];
+
+  if (isNaN(John) == false || isNaN(Ann) == false || isNaN(Pete) == false) {
+    return salaries.John + salaries.Ann + salaries.Pete;
+  } else {
+    return 0;
+  }
 }
