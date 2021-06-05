3b8f230fafedd176f83eddf511820770f6c9a6c0
ann wi <vasileva.ana@outlook.com>
2021-06-05 10:13:34 +0300

    написала функцию getMinMax и функцию showSalary

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,10 @@
 function showSalary(users, age) {
-  // ваш код...
+  let userAge = users.filter((user) => {
+    return user.age <= age;
+  });
+
+  let result = userAge.map((user21) => {
+    return `${user21.name}, ${user21.balance}`;
+  });
+  return result.join("\n");
 }
