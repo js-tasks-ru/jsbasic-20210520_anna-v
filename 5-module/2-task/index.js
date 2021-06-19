4f755785f2f77436c5d7f0e03ea4dbf0b2153ca3
ann wi <vasileva.ana@outlook.com>
2021-06-19 17:12:01 +0300

    написала функцию hideSelf и toggleText

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector(".toggle-text-button");
+  button.onclick = function () {
+    let text = document.getElementById("text");
+
+    if (text.hidden == false) {
+      return (text.hidden = true);
+    } else {
+      return (text.hidden = false);
+    }
+  };
 }
